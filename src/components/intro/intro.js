0ca09db839c2973d93914c719ffbaed2f755c86e
Jonathan Di Bastiano <137315756+JonyDibas@users.noreply.github.com>
2024-09-03 09:51:15 -0300

    act

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,19 +1,27 @@
 import React from "react";
 import './intro.css';
 import Selfie from '../../images/Jpen.png'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import CV from './CV.pdf'
+
 
 const intro = () => {
     return (
-        <section id="intro">
-            <div className="introContent">
-                <span className="hello">Hi, </span>
-                <span className="introText">I'm <span className="introName">Jonathan Di Bastiano</span> <br /> Computer technician and web programmer</span>
-                <p className="introText">Student of the Bachelor's Degree in Systems at the National University of La Plata, Buenos Aires</p>
+        <div>
+            <section id="intro">
+                <div className="introContent">
+                    <span className="hello">Hi, </span>
+                    <span className="introText">I'm <span className="introName">Jonathan Di Bastiano</span> <br /> Computer technician and web programmer</span>
+                    <p className="introText">Student of the Bachelor's Degree in Systems at the National University of La Plata, Buenos Aires</p>
+                </div>
+                <div className="selfie">
+                    <img src={Selfie} alt="Logo" className='selfie' />
+                </div>
+            </section>
+            <div className="button">
+                <button href={CV.pdf} type="button" class="btn btn-primary" download="Jonathan Di Bastiano CV.pdf">Descargar CV</button>
             </div>
-            <div className="selfie">
-                <img src={Selfie} alt="Logo" className='selfie' />
-            </div>
-        </section>
+        </div>
     )
 }
 
